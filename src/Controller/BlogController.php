9d962ba8c24dd288cef356fb99ee89698615e97c
ouerghi <ouerghi.mahdi@gmail.com>
2020-03-02 20:08:13 +0100

    feat: add advert controller

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index()
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+    public function test( Environment $twig) {
+        return new Response("salut");
+    }
+}
